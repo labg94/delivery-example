4af995cce5655aa07ccd5c8aa45cd276ef56f1c3
Luis Benavides <luisalejobg@gmail.com>
2022-02-10 10:27:23 -0300

    🎉 first commit

---
+++ src/main/java/com/example/deliveryexample/DeliveryExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.deliveryexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/deliveryexample/DeliveryExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deliveryexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
